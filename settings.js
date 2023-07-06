b1a82b0cad8116cbe0654072fa160cf4f1ec7ee7
irfan <103992545+Irms237@users.noreply.github.com>
2023-07-06 23:41:39 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,12 +1,12 @@
 const fs = require('fs')
 
-global.creator = 'Zeeoneofc'// yourname
-global.MONGO_DB_URI = "" //database mongodb 
+global.creator = 'Irfan'// yourname
+global.MONGO_DB_URI = "mongodb+srv://<username>:<password>@cluster0.eopqapv.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "" //email
-global.email_password = "" //application password email
+global.your_email = "maulanasaputrairfan8@gmail.com" //email
+global.email_password = "zgoqmirfaxsqxmuk" //application password email
 global.limitCount = 10000
-global.YUOR_PORT = 8000
+global.YUOR_PORT = 1270
 global.loghandler = {
 	noapikey:{
 		status: 403,
